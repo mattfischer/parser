97ebdd43baa43da8ff4ada1ed7e5e242cbcac01c
Matt Fischer <mattfischer84@gmail.com>
2021-05-03 21:13:16 -0500

    Move Tokenizer::Stream impl into .cpp file

--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -26,3 +26,88 @@ unsigned int Tokenizer::patternValue(const std::string &name, unsigned int confi
 
     return kInvalidTokenValue;
 }
+
+Tokenizer::Stream::Stream(const Tokenizer &tokenizer, std::istream &input)
+: mTokenizer(tokenizer), mInput(input)
+{
+    mConsumed = 0;
+    mLine = 0;
+    mConfiguration = 0;
+    mNextToken = {kInvalidTokenValue, 0, 0};
+}
+
+void Tokenizer::Stream::setConfiguration(unsigned int configuration)
+{
+    if(configuration < mTokenizer.mConfigurations.size()) {
+        mConfiguration = configuration;
+    }
+}
+
+unsigned int Tokenizer::Stream::configuration() const
+{
+    return mConfiguration;
+}
+
+const Tokenizer::Token &Tokenizer::Stream::nextToken()
+{
+    if(mLine == 0) {
+        consumeToken();
+    }
+    return mNextToken;
+}
+
+void Tokenizer::Stream::consumeToken()
+{
+    if(mNextToken.value == kErrorTokenValue || mNextToken.value == mTokenizer.mEndValue) {
+        return;
+    }
+
+    bool repeat = true;
+    while(repeat) {
+        while(mConsumed >= mCurrentLine.size()) {
+            if(mConsumed == mCurrentLine.size() && mTokenizer.mNewlineValue != kInvalidTokenValue && mLine > 0) {
+                mNextToken.value = mTokenizer.mNewlineValue;
+                mNextToken.start = mConsumed;
+                mNextToken.line = mLine;
+                mNextToken.text = "<newline>";
+                mConsumed++;
+                return;
+            }
+
+            if(mInput.eof() || mInput.fail()) {
+                mNextToken.value = mTokenizer.mEndValue;
+                mNextToken.start = mConsumed;
+                mNextToken.line = mLine;
+                mNextToken.text = "<end>";
+                return;
+            }
+
+            std::getline(mInput, mCurrentLine);
+            mConsumed = 0;
+            mLine++;
+        }
+
+        unsigned int pattern;
+        unsigned int matched = mTokenizer.mMatchers[mConfiguration]->match(mCurrentLine, mConsumed, pattern);
+        if(matched == 0) {
+            mNextToken.value = kErrorTokenValue;
+            mNextToken.start = mConsumed;
+            mNextToken.line = mLine;
+            mNextToken.text = mCurrentLine.substr(mConsumed, 1);
+
+            repeat = false;
+        } else {
+            TokenValue value = mTokenizer.mConfigurations[mConfiguration].patterns[pattern].value;
+            if(value != kInvalidTokenValue) {
+                mNextToken.value = value;
+                mNextToken.start = mConsumed;
+                mNextToken.line = mLine;
+                mNextToken.text = mCurrentLine.substr(mConsumed, matched);
+
+                repeat = false;
+            }
+
+            mConsumed += matched;
+        }
+    }
+}
--- Tokenizer.hpp
+++ Tokenizer.hpp
@@ -38,90 +38,13 @@ public:
     class Stream
     {
     public:
-        Stream(const Tokenizer &tokenizer, std::istream &input)
-        : mTokenizer(tokenizer), mInput(input)
-        {
-            mConsumed = 0;
-            mLine = 0;
-            mConfiguration = 0;
-            mNextToken = {kInvalidTokenValue, 0, 0};
-        }
+        Stream(const Tokenizer &tokenizer, std::istream &input);
 
-        void setConfiguration(unsigned int configuration)
-        {
-            if(configuration < mTokenizer.mConfigurations.size()) {
-                mConfiguration = configuration;
-            }
-        }
+        void setConfiguration(unsigned int configuration);
+        unsigned int configuration() const;
 
-        unsigned int configuration() const
-        {
-            return mConfiguration;
-        }
-
-        const Token &nextToken()
-        {
-            if(mLine == 0) {
-                consumeToken();
-            }
-            return mNextToken;
-        }
-
-        void consumeToken()
-        {
-            if(mNextToken.value == kErrorTokenValue || mNextToken.value == mTokenizer.mEndValue) {
-                return;
-            }
-
-            bool repeat = true;
-            while(repeat) {
-                while(mConsumed >= mCurrentLine.size()) {
-                    if(mConsumed == mCurrentLine.size() && mTokenizer.mNewlineValue != kInvalidTokenValue && mLine > 0) {
-                        mNextToken.value = mTokenizer.mNewlineValue;
-                        mNextToken.start = mConsumed;
-                        mNextToken.line = mLine;
-                        mNextToken.text = "<newline>";
-                        mConsumed++;
-                        return;
-                    }
-
-                    if(mInput.eof() || mInput.fail()) {
-                        mNextToken.value = mTokenizer.mEndValue;
-                        mNextToken.start = mConsumed;
-                        mNextToken.line = mLine;
-                        mNextToken.text = "<end>";
-                        return;
-                    }
-
-                    std::getline(mInput, mCurrentLine);
-                    mConsumed = 0;
-                    mLine++;
-                }
-
-                unsigned int pattern;
-                unsigned int matched = mTokenizer.mMatchers[mConfiguration]->match(mCurrentLine, mConsumed, pattern);
-                if(matched == 0) {
-                    mNextToken.value = kErrorTokenValue;
-                    mNextToken.start = mConsumed;
-                    mNextToken.line = mLine;
-                    mNextToken.text = mCurrentLine.substr(mConsumed, 1);
-
-                    repeat = false;
-                } else {
-                    TokenValue value = mTokenizer.mConfigurations[mConfiguration].patterns[pattern].value;
-                    if(value != kInvalidTokenValue) {
-                        mNextToken.value = value;
-                        mNextToken.start = mConsumed;
-                        mNextToken.line = mLine;
-                        mNextToken.text = mCurrentLine.substr(mConsumed, matched);
-
-                        repeat = false;
-                    }
-
-                    mConsumed += matched;
-                }
-            }
-        }
+        const Token &nextToken();
+        void consumeToken();
 
     private:
         const Tokenizer &mTokenizer;
