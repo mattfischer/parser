80121ead9855281b67d98bb6ac9cb3f9aa49f23a
Matt Fischer <mattfischer84@gmail.com>
2021-05-08 17:53:55 -0500

    Clean up Earley partitioning code

--- Parser/Earley.hpp
+++ Parser/Earley.hpp
@@ -6,6 +6,7 @@
 
 #include <set>
 #include <vector>
+#include <algorithm>
 
 namespace Parser
 {
@@ -54,95 +55,14 @@ namespace Parser
             std::unique_ptr<ParseData> parseRhs(const std::vector<std::set<Earley::Item>> &completedSets, unsigned int rule, unsigned int rhs, unsigned int start, unsigned int end) const
             {
                 const Grammar::RHS &rhsSymbols = mParser.mGrammar.rules()[rule].rhs[rhs];
-                std::vector<std::vector<unsigned int>> partitions;
-                for(unsigned int i = rhsSymbols.size() - 1; i >= 0; i--) {
-                    const Grammar::Symbol &symbol = rhsSymbols[i];
-                    unsigned int ri = rhsSymbols.size() - 1 - i;
-                    if(ri == 0) {
-                        switch(symbol.type) {
-                            case Grammar::Symbol::Type::Terminal:
-                                partitions.push_back(std::vector<unsigned int>{end - 1});
-                                break;
-                            case Grammar::Symbol::Type::Epsilon:
-                                partitions.push_back(std::vector<unsigned int>{end});
-                                break;
-                            case Grammar::Symbol::Type::Nonterminal:
-                                for(const auto &item : completedSets[end]) {
-                                    if(item.rule == symbol.index && item.start >= start) {
-                                        partitions.push_back(std::vector<unsigned int>{item.start});
-                                    }
-                                }
-                                break;
-                        }
-                    } else {
-                        bool repeat = false;
-                        for(unsigned int j=0; j<partitions.size() || repeat; j++) {
-                            if(repeat) {
-                                j--;
-                                repeat = false;
-                            }
-
-                            if(partitions[j].size() == ri + 1) {
-                                break;
-                            }
-                        
-                            unsigned int currentEnd = partitions[j].back();
-                            switch(symbol.type) {
-                                case Grammar::Symbol::Type::Terminal:
-                                    partitions[j].push_back(currentEnd - 1);
-                                    break;
-                                case Grammar::Symbol::Type::Epsilon:
-                                    partitions[j].push_back(currentEnd);
-                                    break;
-                                case Grammar::Symbol::Type::Nonterminal:
-                                {
-                                    bool first = true;
-                                    bool found = false;
-                                    for(const auto &item : completedSets[currentEnd]) {
-                                        if(item.rule == symbol.index && item.start >= start) {
-                                            found = true;
-                                            if(first) {
-                                                partitions[j].push_back(item.start);
-                                                first = false;
-                                            } else {
-                                                partitions.push_back(partitions[j]);
-                                                partitions.back().push_back(item.start);
-                                            }
-                                        }
-                                    }
-                                    if(!found) {
-                                        partitions.erase(partitions.begin() + j);
-                                        repeat = true;
-                                    }
-                                    break;
-                                }
-                            }
-                        } 
-                    }
-
-                    if(partitions.size() == 0) {
-                        return std::unique_ptr<ParseData>();
-                    }
-
-                    if(i == 0) {
-                        break;
-                    }
-                }
+                std::vector<std::vector<unsigned int>> partitions = findPartitions(completedSets, rule, rhs, start, end);
 
                 for(const auto &partition : partitions) {
-                    if(partition[partition.size() - 1] != start) {
-                        continue;
-                    }
-
                     for(unsigned int j = 0; j<partition.size(); j++) {
-                        unsigned int ji = partition.size() - 1 - j;
+                        unsigned int ji = (unsigned int)(partition.size() - 1 - j);
                         unsigned int pstart = partition[ji];
-                        unsigned int pend;
-                        if(j == partition.size() - 1) {
-                            pend = end;
-                        } else {
-                            pend = partition[ji - 1];
-                        }
+                        unsigned int pend = (j == partition.size() - 1) ? end : partition[ji - 1];
+
                         if(rhsSymbols[j].type == Grammar::Symbol::Type::Nonterminal) {
                             parseRule(completedSets, rhsSymbols[j].index, pstart, pend);
                         }
@@ -151,53 +71,65 @@ namespace Parser
                 return std::unique_ptr<ParseData>();
             }
 
-            std::unique_ptr<ParseData> parseRule2(const std::vector<std::set<Earley::Item>> &completedSets, unsigned int rule, unsigned int end, unsigned int &start, bool &found) const
+            std::vector<unsigned int> findStarts(const std::vector<std::set<Earley::Item>> &completedSets, const Grammar::Symbol &symbol, unsigned int end, unsigned int minStart) const
             {
-                found = false;
-                for(const auto &item : completedSets[end]) {
-                    if(item.rule != rule) {
-                        continue;
-                    }
+                std::vector<unsigned int> starts;
 
-                    start = item.start;
-                    const Grammar::RHS &rhs = mParser.mGrammar.rules()[item.rule].rhs[item.rhs];
-                    unsigned int i = (unsigned int)(rhs.size() - 1);
-                    unsigned int p = end;
-                    while(true) {
-                        bool valid = true;
-                        switch(rhs[i].type) {
-                            case Grammar::Symbol::Type::Terminal:
-                                p--;
-                                break;
-                            case Grammar::Symbol::Type::Nonterminal:
-                            {
-                                unsigned int s;                   
-                                bool f;
-                                std::unique_ptr<ParseData> d = parseRule(completedSets, rhs[i].index, p, s, f);
-                                if(!f) {
-                                    valid = false;
-                                    break;
-                                }
-                                p = s - 1;
-                                break;
+                switch(symbol.type) {
+                    case Grammar::Symbol::Type::Terminal:
+                        starts.push_back(end - 1);
+                        break;
+                    case Grammar::Symbol::Type::Epsilon:
+                        starts.push_back(end);
+                        break;
+                    case Grammar::Symbol::Type::Nonterminal:
+                        for(const auto &item : completedSets[end]) {
+                            if(item.rule == symbol.index && item.start >= minStart) {
+                                starts.push_back(item.start);
                             }
-                            case Grammar::Symbol::Type::Epsilon:
-                                break;
                         }
-                        
-                        if(!valid || i == 0) {
-                            break;
-                        } else {
-                            i--;
+                        break;
+                }
+
+                return starts;               
+            }
+
+            std::vector<std::vector<unsigned int>> findPartitions(const std::vector<std::set<Earley::Item>> &completedSets, unsigned int rule, unsigned int rhs, unsigned int start, unsigned int end) const
+            {
+                const Grammar::RHS &rhsSymbols = mParser.mGrammar.rules()[rule].rhs[rhs];
+                std::vector<std::vector<unsigned int>> partitions;
+
+                for(unsigned int i = 0; i<rhsSymbols.size(); i++) {
+                    unsigned int ri = (unsigned int)(rhsSymbols.size() - 1 - i);
+                    const Grammar::Symbol &symbol = rhsSymbols[ri];
+                    if(i == 0) {
+                        std::vector<unsigned int> starts = findStarts(completedSets, symbol, end, start);
+                        for(unsigned int s : starts) {
+                            partitions.push_back(std::vector<unsigned int>{s});
                         }
+                    } else {
+                        std::vector<std::vector<unsigned int>> oldPartitions = std::move(partitions);
+                        bool repeat = false;
+                        for(unsigned int j=0; j<oldPartitions.size(); j++) {
+                            unsigned int currentEnd = oldPartitions[j].back();
+                            std::vector<unsigned int> starts = findStarts(completedSets, symbol, currentEnd, start);
+                            for(unsigned int s : starts) {
+                                partitions.push_back(oldPartitions[j]);
+                                partitions.back().push_back(s);
+                            }
+                        } 
                     }
 
-                    if(p == start) {
-                        found = true;
+                    if(partitions.size() == 0) {
                         break;
                     }
                 }
-                return std::unique_ptr<ParseData>();
+
+                partitions.erase(std::remove_if(partitions.begin(), partitions.end(), [&](const std::vector<unsigned int> &partition) {
+                    return partition.back() != start;
+                }), partitions.end());
+
+                return partitions;
             }
 
         private:
